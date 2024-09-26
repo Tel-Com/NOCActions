bac7a94c77eb2f39c5781ef8b1ec72fd7bc0dbe4
Fabiano <contatofasantiago@hotmail.com>
2024-09-26 13:09:34 -0300

    retirado comentários instantâneos

--- Program.cs
+++ Program.cs
@@ -4,21 +4,14 @@
  * Data: 08/07/2024
  * Hora: 06:37
  * 
- * Para alterar este modelo use Ferramentas | Opções | Codificação | Editar Cabeçalhos Padrão.
  */
 using System;
 using System.Windows.Forms;
 
 namespace NOCActions
 {
-	/// <summary>
-	/// Class with program entry point.
-	/// </summary>
 	internal sealed class Program
 	{
-		/// <summary>
-		/// Program entry point.
-		/// </summary>
 		[STAThread]
 		private static void Main(string[] args)
 		{
